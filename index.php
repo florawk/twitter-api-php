375f90cf65eccac5c759eadf85be1ec1faa3cc0a
FloraWK <flora@yellowcube.com>
2013-09-30 12:08:05 +0800

    print out attributes and variable types of EACH tweet.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require_once('TwitterAPIExchange.php');
 
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
-    'oauth_access_token' => "",
-    'oauth_access_token_secret' => "",
-    'consumer_key' => "",
-    'consumer_secret' => ""
+    'oauth_access_token' => "1903237716-XhZqg7Yg7NmBh6n5W4DxYihwBKtp0awkoU1brtL",
+    'oauth_access_token_secret' => "uMYog5h4qfSBsd5pWnoDqiuRkJiXNrYuxVNTDqwAkc",
+    'consumer_key' => "bnPvZVHDS5CErezIeEgg",
+    'consumer_secret' => "D2Jd7cz88GHS2ZObYYEzHUhpw2tkrcQ045W3PBzXb6E"
 );
 
 /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
@@ -22,9 +22,9 @@ $postfields = array(
 
 /** Perform a POST request and echo the response **/
 $twitter = new TwitterAPIExchange($settings);
-echo $twitter->buildOauth($url, $requestMethod)
-             ->setPostfields($postfields)
-             ->performRequest();
+// echo 'POST<br />'.$twitter->buildOauth($url, $requestMethod)
+//              ->setPostfields($postfields)
+//              ->performRequest();
 
 /** Perform a GET request and echo the response **/
 /** Note: Set the GET field BEFORE calling buildOauth(); **/
@@ -32,6 +32,33 @@ $url = 'https://api.twitter.com/1.1/followers/ids.json';
 $getfield = '?screen_name=J7mbo';
 $requestMethod = 'GET';
 $twitter = new TwitterAPIExchange($settings);
-echo $twitter->setGetfield($getfield)
+// echo '<br /><br />GET<br />'.$twitter->setGetfield($getfield)
+//              ->buildOauth($url, $requestMethod)
+//              ->performRequest();
+
+/** Search for hashtags **/
+$url = 'https://api.twitter.com/1.1/search/tweets.json';
+$getfield = '?q=%23cisco&result_type=mixed&count=4';
+$requestMethod = 'GET';
+$twitter = new TwitterAPIExchange($settings);
+
+$result = $twitter->setGetfield($getfield)
              ->buildOauth($url, $requestMethod)
              ->performRequest();
+
+$myArray = json_decode($result, true);
+echo 'Number of Tweets: '.sizeof($myArray["statuses"]).'<br /><br />';
+
+foreach($myArray["statuses"] as $one_tweet)
+{
+    // echo '<pre>'; var_dump($one_tweet); echo '</pre>'; 
+    echo '<ol>';
+    foreach($one_tweet as $key => $value)
+    {
+        echo '<li>';
+        echo $key.': ';
+        echo (gettype($value) == 'array')? '<span style="color:red;">array</span>'.'<br />':gettype($value).'<br />';
+        echo '</li>';
+    }
+    echo '</ol>///////////////////////////////////<br />';
+}
